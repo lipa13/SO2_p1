f26fed012dd674046a86731651fb3913c385afbd
lipa13 <jaklip04@gmail.com>
2025-03-16 23:38:02 +0100

    plan for the algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,47 @@
 
 using namespace std;
 
-void think()
+void think(int id)
 {
-
+    cout << "Philospher " << id << " thinks\n";
+    this_thread::sleep_for(chrono::milliseconds(500));
 }
 
-// 
-void eat()
+void eat(int id)
 {
-
+    cout << "Philosopher " << id << " eats...\n";
+    this_thread::sleep_for(chrono::milliseconds(500));
 }
 
-void philosopher_job()
+void philosopher_job(int id, int n)
 {
+    int left_fork = id;
+    int right_fork = (id+1)%n;
 
+    while(true)
+    {
+        // filzof myśli
+        // filozof próbuje pobrać 1. widelec (o niższym numerze)
+        // 2. widelec
+        // filozof je
+        // filozof odkłada widelce w tej samej kolejności w jakiej je wziął
+        // itd 
+    }  
 }
 
 // function that runs the algorithm
 void run(int n)
 {
-    vector<thread> philosophers;
-
     // creating the vector of threads
+    vector<thread> philosophers;
     for (int i=0; i <n; i++) 
     {
-        philosophers.emplace_back(philosopher_job, i);
+        philosophers.emplace_back(philosopher_job, i, n);
+    }
+
+    for (auto &p : philosophers)
+    {
+        p.join();
     }
 }
 
