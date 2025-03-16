e7f2eb2e185664931e12a46ed0c57a345d02f975
lipa13 <jaklip04@gmail.com>
2025-03-16 22:37:29 +0100

    basic main.cpp structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <thread>
 
 using namespace std;
 
+void think()
+{
+
+}
+
+// 
+void eat()
+{
+
+}
+
+void philosopher_job()
+{
+
+}
+
+// function that runs the algorithm
+void run(int n)
+{
+    vector<thread> philosophers;
+
+    // creating the vector of threads
+    for (int i=0; i <n; i++) 
+    {
+        philosophers.emplace_back(philosopher_job, i);
+    }
+}
+
 int main()
 {
-    cout << "Hello, from SO2_p1!\n";
+    int n;
+
+    cout << "Number of philosophers: ";
+    cin >> n;
+
+    run(n);
+
     return 0;
 }
