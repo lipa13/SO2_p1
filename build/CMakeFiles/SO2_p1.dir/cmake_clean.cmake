abe358beb92b8aea24bfef2d494169c02c717efa
lipa13 <jaklip04@gmail.com>
2025-03-16 21:26:51 +0100

    first commit

---
+++ build/CMakeFiles/SO2_p1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SO2_p1.dir/src/main.cpp.obj"
+  "CMakeFiles/SO2_p1.dir/src/main.cpp.obj.d"
+  "SO2_p1.exe"
+  "SO2_p1.exe.manifest"
+  "SO2_p1.pdb"
+  "libSO2_p1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SO2_p1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, from SO2_p1!\n";
+    return 0;
+}
